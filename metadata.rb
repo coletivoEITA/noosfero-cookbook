aa9dd056ca2854be7a9b4d9763a6f63f8bca3422
Braulio Bhavamitra <braulio@eita.org.br>
2014-05-16 17:06:43 +0000

    Daemonize unicorn (obviously!)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'braulio@eita.org.br'
 license          'GPLv3+'
 description      'Install/configure Noosfero social-economic network'
 long_description File.read("#{File.dirname __FILE__}/README.md")
-version          '1.0.1'
+version          '1.0.2'
 
 %w{ debian ubuntu }.each do |os|
   supports os
