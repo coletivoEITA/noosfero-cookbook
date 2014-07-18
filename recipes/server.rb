42ef72b8f86180cdeb8707e10b2376397d662e7a
Braulio Bhavamitra <braulio@eita.org.br>
2014-07-17 23:03:00 -0300

    Fix apache support

--- recipes/server.rb
+++ recipes/server.rb
@@ -23,23 +23,25 @@ case node[:noosfero][:server][:proxy]
 when 'nginx'
   include_recipe 'nginx'
 
-  template "#{node[:nginx][:dir]}/sites-enabled/#{node[:noosfero][:service_name]}" do
+  template "#{node[:nginx][:dir]}/sites-available/#{node[:noosfero][:service_name]}" do
     source "nginx.conf.erb"
     owner node[:nginx][:user]
     group node[:nginx][:group]
     variables node[:noosfero]
-    notifies :reload, "service[nginx]"
-    action :create
+  end
+  nginx_site node[:noosfero][:service_name] do
+    enable true
   end
 when 'apache'
   include_recipe 'apache2'
 
-  web_app node[:noosfero][:service_name] do
-    enable true
-
-    node[:noosfero].each do |key, value|
-      send key, value
-    end
+  template "#{node[:apache][:dir]}/sites-available/#{node[:noosfero][:service_name]}" do
+    source "apache2.conf.erb"
+    owner node[:apache][:user]
+    group node[:apache][:group]
+    variables node[:noosfero]
   end
-  notifies :reload, "service[apache2]"
+  apache_site node[:noosfero][:service_name] do
+    enable true
+  end
 end
