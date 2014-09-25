b12cc8902a8d770ad58fc41e909cf1abf3dc4346
Braulio Bhavamitra <braulio@eita.org.br>
2014-09-25 08:14:30 -0300

    Add pidgin-data as dependency

--- attributes/default.rb
+++ attributes/default.rb
@@ -47,8 +47,9 @@ default[:noosfero][:dependencies_with] = 'quick_start'
 
 case node[:platform_family]
 when 'debian', 'ubuntu'
-  default[:noosfero][:packages_for_packages] = %w[ ruby rake po4a libgettext-ruby-util libgettext-ruby1.8 libsqlite3-ruby rcov librmagick-ruby libredcloth-ruby libhpricot-ruby libwill-paginate-ruby iso-codes libfeedparser-ruby libdaemons-ruby thin tango-icon-theme ]
-  default[:noosfero][:packages_for_bundler] = %w[ po4a iso-codes tango-icon-theme curl libmagickwand-dev libpq-dev libreadline-dev libsqlite3-dev libxslt1-dev ]
+  default[:noosfero][:base_packages] = 'po4a iso-codes tango-icon-theme pidgin-data'
+  default[:noosfero][:packages_for_packages] = default[:noosfero][:base_packages] + %w[ ruby rake libgettext-ruby-util libgettext-ruby1.8 libsqlite3-ruby rcov librmagick-ruby libredcloth-ruby libhpricot-ruby libwill-paginate-ruby libfeedparser-ruby libdaemons-ruby thin  ]
+  default[:noosfero][:packages_for_bundler] = default[:noosfero][:base_packages] + %w[ curl libmagickwand-dev libpq-dev libreadline-dev libsqlite3-dev libxslt1-dev ]
   default[:noosfero][:packages_for_quick_start] = %w[]
 end
 
