328b3466abf8d85197ccce4b0fa94fe056b4b5dd
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-06 20:59:38 -0200

    Set default for nginx spdy

--- attributes/ssl.rb
+++ attributes/ssl.rb
@@ -5,3 +5,7 @@ default[:noosfero][:ssl][:default] = true
 default[:noosfero][:ssl][:spdy] = node[:noosfero][:ssl][:enable]
 default[:noosfero][:ssl][:redirect_http] = true
 
+if node[:noosfero][:ssl][:spdy]
+  default[:nginx][:default][:modules] = "http_spdy_module"
+  default[:nginx][:source][:modules] = "nginx::http_spdy_module"
+end
