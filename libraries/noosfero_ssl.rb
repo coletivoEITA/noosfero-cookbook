e4bd567abd31410c639a7240d7b6c66713407383
Braulio Bhavamitra <braulio@eita.org.br>
2015-02-11 23:19:59 -0200

    Add missed ssl attributes

--- libraries/noosfero_ssl.rb
+++ libraries/noosfero_ssl.rb
@@ -10,8 +10,13 @@ class Chef
     attribute :default, kind_of: Boolean, default: true
     attribute :spdy, kind_of: Boolean, default: true
     attribute :redirect_http, kind_of: Boolean, default: true
+
     # default to only safe protocols
     attribute :protocols, kind_of: String, default: "TLSv1 TLSv1.1 TLSv1.2"
+
+    attribute :certificate, kind_of: String, default: nil
+    attribute :certificate_key, kind_of: String, default: nil
+    attribute :certificate_chain, kind_of: String, default: nil
   end
 
   class Provider::NoosferoSsl < NoosferoProvider
