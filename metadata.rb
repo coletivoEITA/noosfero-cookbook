53d44712deaa0b63b817ce54c74f6a02edc2290a
Braulio Bhavamitra <braulio@eita.org.br>
2015-05-12 08:50:49 -0300

    init: workaround problem with su -l

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'braulio@eita.org.br'
 license          'GPLv3+'
 description      'Install/configure Noosfero social-economic network'
 long_description File.read("#{File.dirname __FILE__}/README.md")
-version          '3.2.0'
+version          '3.2.1'
 
 %w{ debian ubuntu }.each do |os|
   supports os
